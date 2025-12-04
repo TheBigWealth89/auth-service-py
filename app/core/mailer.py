be620fd4ee54b9fdbef90054fdb4b8cb07025997
Dabira Olaoluwa <dev373055@gmail.com>
2025-12-04 08:02:02 -0800

    feat: update my db

--- app/core/mailer.py
+++ app/core/mailer.py
@@ -8,11 +8,45 @@ class ResendMailer:
         resend.api_key = self.api_key
 
     async def send_verification_email(self, email: str, token: str):
+
         link = f"http://localhost:8000/auth/verify-email?token={token}"
+
         print(f"link: {link}")
         resend.Emails.send({
             "from": "Auth-Service <onboarding@resend.dev>",
             "to": email,
             "subject": "Verify Your Email",
-            "html": f"<p>Click to verify:</p><a href='{link}'>Verify Email</a>"
+            "html": f"""
+    <div style="font-family: Arial, sans-serif; line-height: 1.6;">
+        <h2>Welcome to Auth-Service 🎉</h2>
+        <p>Thanks for creating an account! Before you can start using it, we just need to confirm your email address.</p>
+        
+        <p>
+            <a href="{link}" 
+               style="
+                   background-color: #4CAF50;
+                   color: white;
+                   padding: 10px 16px;
+                   text-decoration: none;
+                   border-radius: 6px;
+                   display: inline-block;
+                   font-weight: bold;
+               ">
+                Verify Email
+            </a>
+        </p>
+
+        <p>If the button above doesn't work, copy and paste this link into your browser:</p>
+        <p style="word-break: break-all;">
+            {link}
+        </p>
+
+        <hr>
+
+        <p style="font-size: 12px; color: #555;">
+            This link will expire in 10 minutes for security reasons.
+            If you didn’t request this, you can safely ignore the email.
+        </p>
+    </div>
+"""
         })
