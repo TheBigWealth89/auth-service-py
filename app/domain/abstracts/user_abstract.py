f8baa7597033bb4a871a56a20cc7840dd5134bed
Dabira Olaoluwa <dev373055@gmail.com>
2025-12-15 02:49:19 -0800

    feat: add get user by id abstract

--- app/domain/abstracts/user_abstract.py
+++ app/domain/abstracts/user_abstract.py
@@ -3,6 +3,7 @@ from typing import Optional
 from ...models.user_model import User
 from ...schema.user_dto import UserCreateDTO
 
+
 class IUserRepository(ABC):
     @abstractmethod
     async def get_user_by_email(self, email: str) -> Optional[User]:
@@ -10,8 +11,11 @@ class IUserRepository(ABC):
         raise NotImplementedError
 
     @abstractmethod
+    async def get_user_by_id(self, user_id: int) -> Optional[User]:
+        """Return a User or None by user_id"""
+        raise NotImplementedError
+
+    @abstractmethod
     async def create_user(self, user_create: UserCreateDTO, password_hash: str) -> User:
         """Create a user and return the created User."""
         raise NotImplementedError
-
-
