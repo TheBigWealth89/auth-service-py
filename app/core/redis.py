7dc5e56e8220fae989ff82f680271491f8be9363
Dabira Olaoluwa <dev373055@gmail.com>
2026-07-14 14:04:34 -0700

    j

--- app/core/redis.py
+++ app/core/redis.py
@@ -3,7 +3,7 @@ from .config import REDIS_URL
 
 # Create a global redis connection pool
 redis_client = redis.from_url(REDIS_URL, decode_responses=True)
-
+# get redis client 
 async def get_redis():
     """FastAPI Dependency for Redis"""
     yield redis_client
